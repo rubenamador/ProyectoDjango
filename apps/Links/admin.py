e7331894ece161bee29c3c3282ecd3019b3867c1
rubenamador <ruben.amador.madrid@gmail.com>
2021-01-19 17:45:07 +0100

    Creacion de modelos de llamadas y reuniones en la aplicacion Links

--- apps/Links/admin.py
+++ apps/Links/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from apps.Links.models import Call, Meeting
+
 # Register your models here.
+admin.site.register(Call)
+admin.site.register(Meeting)
--- apps/Links/models.py
+++ apps/Links/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+from apps.Nodes.models import Person, Phone, MeetingPoint
+
 # Create your models here.
+
+class Call(models.Model):
+	id = models.CharField(max_length=25, primary_key=True)
+	phone_1 = models.OneToOneField(Phone, related_name="phone_1+", null=True, blank=True, on_delete=models.CASCADE)
+	phone_2 = models.OneToOneField(Phone, related_name="phone_2+", null=True, blank=True, on_delete=models.CASCADE)
+	
+	class Meta:
+		ordering = ['id']
+		
+	def __str__(self):
+		return '{}'.format(self.id)
+
+class Meeting(models.Model):
+	id = models.CharField(max_length=25, primary_key=True)
+	person = models.OneToOneField(Person, related_name="person+", null=True, blank=True, on_delete=models.CASCADE)
+	point = models.OneToOneField(MeetingPoint, related_name="point+", null=True, blank=True, on_delete=models.CASCADE)
+	
+	class Meta:
+		ordering = ['id']
+		
+	def __str__(self):
+		return '{}'.format(self.id)
