e719d78b1c3b297e699cd0a8c9064ada696b320a
rubenamador <ruben.amador.madrid@gmail.com>
2021-01-19 17:43:03 +0100

    Creacion de modelos de personas, telefonos y lugares de reunion en la aplicacion Nodes

--- apps/Nodes/admin.py
+++ apps/Nodes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.Nodes.models import Person, Phone, MeetingPoint
+
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Phone)
+admin.site.register(MeetingPoint)
--- apps/Nodes/models.py
+++ apps/Nodes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+	id = models.CharField(max_length=25, primary_key=True)
+	name = models.CharField(max_length=25, default='')
+	surname = models.CharField(max_length=25, default='')
+	
+	class Meta:
+		ordering = ['name']
+		
+	def __str__(self):
+		return '{}'.format(self.id)
+
+class Phone(models.Model):
+	number = models.CharField(max_length=9, primary_key=True)
+	owner = models.ForeignKey(Person, null=True, blank=True, on_delete=models.CASCADE)
+	
+	class Meta:
+		ordering = ['number']
+		
+	def __str__(self):
+		return '{}'.format(self.number)
+
+class MeetingPoint(models.Model):
+	id = models.CharField(max_length=25, primary_key=True)
+	place = models.CharField(max_length=25, default='')
+	date = models.CharField(max_length=10, default='')
+	time = models.CharField(max_length=5, default='')
+
+	class Meta:
+		ordering = ['id']
+		
+	def __str__(self):
+		return '{}'.format(self.id)
