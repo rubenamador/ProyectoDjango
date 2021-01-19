f914ec561bf284ad1c11926b83931f8277b104ed
rubenamador <ruben.amador.madrid@gmail.com>
2021-01-19 17:33:20 +0100

    Creacion de aplicaciones en proyecto Django

--- TFG/settings.py
+++ TFG/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.Nodes',
+    'apps.Links',
+    'apps.Graphs',
 ]
 
 MIDDLEWARE = [
