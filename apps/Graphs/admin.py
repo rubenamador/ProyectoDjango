2b04a147ef8c525bfb5cd7374cb90833ef915443
rubenamador <ruben.amador.madrid@gmail.com>
2021-01-19 17:47:15 +0100

    Creacion de modelo de grafo en la aplicacion Graphs

--- apps/Graphs/admin.py
+++ apps/Graphs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from apps.Graphs.models import Graph
+
 # Register your models here.
+admin.site.register(Graph)
--- apps/Graphs/models.py
+++ apps/Graphs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from apps.Nodes.models import Person, Phone
+from apps.Links.models import Call, Meeting
+
 # Create your models here.
+
+class Graph(models.Model):
+	id = models.CharField(max_length=25, primary_key=True)
+	
+	class Meta:
+		ordering = ['id']
+	
+	def __str__(self):
+		return '{}'.format(self.id)
