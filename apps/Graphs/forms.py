c4425d1357579ec22b434dabbbd5a198a99810f3
rubenamador <ruben.amador.madrid@gmail.com>
2021-01-21 12:25:52 +0100

    Formularios para representar el grado de un nodo y buscar caminos agregados

--- apps/Graphs/forms.py
+++ apps/Graphs/forms.py
@@ -15,3 +15,51 @@ class GraphForm(forms.ModelForm):
 		widgets = {
 			'id': forms.TextInput(attrs={'class':'form-control'}),
 		}
+
+from apps.Nodes.models import Phone
+
+class PersonForm(forms.ModelForm):
+	
+	class Meta:
+		model = Phone
+		fields = [
+			'owner',
+		]
+		labels = {
+			'owner': 'Owner'
+		}
+		widgets = {
+			'owner': forms.Select(attrs={'class':'form-control'}),
+		}
+
+from apps.Links.models import Call
+
+class PhoneForm(forms.ModelForm):
+	
+	class Meta:
+		model = Call
+		fields = [
+			'phone_1',
+		]
+		labels = {
+			'phone_1': 'Phone_1'
+		}
+		widgets = {
+			'phone_1': forms.Select(attrs={'class':'form-control'}),
+		}
+
+from apps.Links.models import Meeting
+
+class MeetingPointForm(forms.ModelForm):
+	
+	class Meta:
+		model = Meeting
+		fields = [
+			'point',
+		]
+		labels = {
+			'point': 'Point'
+		}
+		widgets = {
+			'point': forms.Select(attrs={'class':'form-control'}),
+		}
